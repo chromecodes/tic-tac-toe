846e76ed4fe6db2c7c6111e387b7ca8ebf8d26f9
chromecodes <hameedhussain782@gmail.com>
2022-07-25 02:06:25 +0530

    added the player functionality

--- script.js
+++ script.js
@@ -1,37 +1,66 @@
 
 const GameBoard = (() => {
-    const board = ["","","","","","","","",""] 
+    const board = ["","","","","","","","",""];
 
     const boardRset = (tiles) => {
-
-        console.log(tiles);
         for( i=0; i<9; i++){
             board[i] = "";
             tiles[i].textContent = "";
-        }
-    }
+        };
+    };
 
     const assignValue = (index, value)=> {
         if(  value == "X" && board[index] !== "X" && board[index] !== "O"){
             board[index] = value;
             player.turns.turnIncr();
-            console.log(board);
-            displayVlue(index, value)
+            player.turns.ttlTurn();
+            displayCtrl.displayVlue(index, value);
+            displayCtrl.displaySts();
             checkStat();
-        }
+        };
         if( value == "O" && board[index] !== "X" && board[index] !== "O"){
             board[index] = value;
             player.turns.turnDecr();
-            console.log(board);
-            displayVlue(index, value);
+            player.turns.ttlTurn();
+            displayCtrl.displayVlue(index, value);
+            displayCtrl.displaySts();
             checkStat();
-        }
+        };
+    };
 
-    }
-    const displayVlue = (index, value) => {
-        const display = document.querySelector(`[data-index="${index}"]`)
-        display.textContent = value;
-    }
+    const displayCtrl = (()=>{
+        const displayVlue = (index, value) => {
+            const display1 = document.querySelector(`[data-index="${index}"]`);
+            display1.textContent = value;
+        };
+
+        const display2 = document.getElementById ("status");
+
+        const displaySts = () =>{
+            if( player.turns.getTurn() == 1) {
+                display2.textContent = `It's Player ${player.creatPlayers.playerX.name}'s Turn`;
+            };
+            if( player.turns.getTurn() == 2) {
+                display2.textContent = `It's Player ${player.creatPlayers.playerO.name}'s Turn`;
+            };
+        };
+
+        const displayWiner = () => {
+            if( player.turns.getTurn() == 2) {
+                display2.textContent = ` Congrat's Player ${player.creatPlayers.playerX.name} you won!!!`;
+                return;
+            } 
+            if( player.turns.getTurn() == 1) {
+                display2.textContent = ` Congrat's Player ${player.creatPlayers.playerO.name} you won!!!`;
+                return;
+            } 
+        }
+        const displayTie = ()=> {
+            display2.textContent = ` WOW it's a tie!!!`;
+        }
+        return {displayVlue, displaySts, displayWiner, displayTie};
+    })();
+   
     const checkStat = () => {
 
         if ( (board[0] === board[1] && board[0] === board[2] && board[0] !== "") ||
@@ -45,63 +74,85 @@ const GameBoard = (() => {
             {
                game.isOver.itIsOver();
                game.rstBtn.playAgn();
-                
-                    
-        }
+               displayCtrl.displayWiner();
+               return;
+        };
+        if( player.turns.geTtl() > 9) {
+            displayCtrl.displayTie();
+        };
     }
-    return {boardRset, assignValue, }
+    return {boardRset, assignValue, displayCtrl};
 })();
 
 const player = (() => {
 
-    const playerX = {
-        name : "one",
-        sign : "X",
-    }
-    const playerO = {
-        name : "two",
-        sign : "O",
-    }
+    const players = (name, sign)=> {
+        return {name, sign};
+    };
+
+    const creatPlayers = (()=> {
+        const playerX = players ( "one", "X");
+        const playerO = players ( "two", "O");
+
+        const renameply = (input1="one", input2="two") => {
+            if (input1 !== ""){
+                playerX.name = input1;
+            }
+            if (input2 !== ""){
+                playerO.name = input2;
+            }
+        }
+        return {playerX,playerO,renameply};
+    })();
+
+
     const turns = (() => {
-        let turn = 1 
+        let turn = 1 ;
+        let tlTurn = 1;
+        const ttlTurn = ()=> {
+            tlTurn++;
+        };
+        const ttlTurnRst = () => {
+            tlTurn = 1;
+        };
+        const geTtl=()=>{
+            return tlTurn;
+        };
         const getTurn = () => {
         return turn;
-        }
+        };
         const turnRset = () => {
             turn = 1;
-        }
+        };
         const turnIncr = () => {
             turn++;
-            console.log(turn);
-        }
+        };
         const turnDecr = () => {
             turn--;
-            console.log(turn);
-        }
-        return{getTurn, turnRset, turnIncr, turnDecr }
+        };
+        return{ttlTurn, ttlTurnRst, geTtl, getTurn, turnRset, turnIncr, turnDecr };
     })();
 
     const getValue = (index) => {
 
-        console.log(turns.getTurn());
         if (turns.getTurn() == 1){
-            if(playerX.sign == "X") {
-             value = playerX.sign;
-            } else if (playerO.sign == "X") {
-             value = playerO.sign;
-            }
-        }
+            if(creatPlayers.playerX.sign == "X") {
+             value = creatPlayers.playerX.sign;
+            } else if (creatPlayers.playerO.sign == "X") {
+             value = creatPlayers.playerO.sign;
+            };
+        };
         if (turns.getTurn() == 2){
-            if(playerX.sign == "O") {
-             value = playerX.sign;
-            } else if (playerO.sign == "O") {
-             value = playerO.sign;
-            }
-        }
+            if(creatPlayers.playerX.sign == "O") {
+             value = creatPlayers.playerX.sign;
+            } else if (creatPlayers.playerO.sign == "O") {
+             value = creatPlayers.playerO.sign;
+            };
+        };
 
-        GameBoard.assignValue(index, value )
+        GameBoard.assignValue(index, value);
     }
-    return {turns, getValue}
+    return {turns, creatPlayers, getValue, creatPlayers};
 })();
 
 
@@ -110,29 +161,28 @@ const game = (() => {
 
     const tiles = document.querySelectorAll(".tile");
 
-
     const isOver = (() => {
         over = false;
         const isTheOver = () => {
             return over;
-        }
+        };
         const itIsOver = () => {
             over = true;
-        }
+        };
         const rstOver = () => {
             over = false;
-        }
+        };
         return { itIsOver, isTheOver,rstOver }
     })();
 
     const addTileLis = (() => {
         tiles.forEach(tile => {tile.addEventListener("click",  (e) => {
-            if(isOver.isTheOver() === false){
-                index = e.target.dataset.index;
-                player.getValue(index);
-            }
-            }) 
-        }) 
+                if(isOver.isTheOver() === false){
+                    index = e.target.dataset.index;
+                    player.getValue(index);
+                };
+            });
+        });
     })();
      
     const rstBtn = (()=> {
@@ -142,16 +192,30 @@ const game = (() => {
             restartBtn.textContent = "Restart"
             GameBoard.boardRset(tiles);
             player.turns.turnRset();
+            player.turns.ttlTurnRst();
             isOver.rstOver();
+            GameBoard.displayCtrl.displaySts();
         });
         const playAgn = ()=> {
             restartBtn.textContent = "Play Again.."
-        } 
-
+        };
         return {playAgn}
     })();
 
+    const start = (()=>{
 
-    return { isOver, rstBtn}
-
+        const startbtn = document.getElementById("start")
+        startbtn.addEventListener("click", () => {
+            const inputs = document.getElementById("inputs");
+            const gameboard = document.getElementById("gameboard");
+            const inputBx1 = document.getElementById("player1");
+            const inputBx2 = document.getElementById("player2");
+            player.creatPlayers.renameply(inputBx1.value, inputBx2.value);
+            GameBoard.displayCtrl.displaySts();
+            console.log('s');
+            inputs.classList.toggle("display");
+            gameboard.classList.toggle("display");
+        });
+    })();
+    return { isOver, rstBtn};
 })();
